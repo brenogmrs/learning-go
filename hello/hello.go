c68058658075ff18cc2d3e6e7e3d1aacab3b84a1
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-02-06 15:56:16 -0300

    working with variables

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+
+	var nome string = "Breno"
+	var vesao float32 = 1.1
+	var idade int
+	
+	fmt.Println("Olá, sr.", nome, "sua é", idade)
+	fmt.Println("Este programa está na versão", vesao)
 }
