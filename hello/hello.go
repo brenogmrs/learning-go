539ff8182c8555f1774bf5aabe1bd7958d1f4a9b
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-02-06 22:41:21 -0300

    working with scan

--- hello/hello.go
+++ hello/hello.go
@@ -4,10 +4,24 @@ import "fmt"
 
 func main() {
 
-	var nome string = "Breno"
-	var vesao float32 = 1.1
-	var idade int
+	var nome = "Breno"
 	
-	fmt.Println("Olá, sr.", nome, "sua é", idade)
+	vesao := 1.1
+
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", vesao)
+
+	fmt.Println("1 - iniciar monitoramento")
+	fmt.Println("2 - exibir logs")
+	fmt.Println("0 - sair do programa")
+
+	var comando int
+	// fmt.Scan() -> infere o tipo
+	fmt.Scan(&comando)
+
+	// fmt.Scanf() -> não infere o tipo e requer um modificador
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("o endereço da variavel comando é", &comando)
+	fmt.Println("o comando escolhido foi,", comando)
 }
