6c47471259424b089ddeebdc96037183e534cd5d
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-02-15 14:50:20 -0300

    implemented AccountOwner struct into checking account struct

--- oo/bank/main.go
+++ oo/bank/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"fmt"
-	accounts "github.com/oo/bank/accounts"
+
+	"github.com/oo/bank/accounts"
+	"github.com/oo/bank/customers"
 )
 
 func main() {
+
+	customer1 := customers.AccountOwner{
+		Name:       "Breno",
+		Cpf:        "123123",
+		Occupation: "it",
+	}
+
+	customer2 := customers.AccountOwner{
+		Name:       "Teste2",
+		Cpf:        "123123",
+		Occupation: "ti",
+	}
+
 	ccAccount := accounts.CheckingAccount{
-		Owner:   "Breno",
-		Balance: 300,
+		Owner: customer1,
 	}
 
 	ccAccount2 := accounts.CheckingAccount{
-		Owner:   "Teste2",
-		Balance: 100,
+		Owner: customer2,
 	}
 
 	status := ccAccount.Transfer(200., &ccAccount2)
