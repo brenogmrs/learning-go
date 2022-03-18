8f7879359558280f80d586a7ffe159e0ca3e6c5e
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-18 12:51:39 -0300

    '''generics'''

---
+++ 10 - interfaces/generics.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func generic(i interface{}) {
+	fmt.Println(i)
+}
+
+func main() {
+	fmt.Println("Generic types ")
+	generic("string")
+	generic(1)
+	generic(true)
+
+	m := map[interface{}]interface{}{
+		1:            "string",
+		float32(200): true,
+		"string":     "string",
+		true:         float64(10),
+	}
+
+	fmt.Println(m)
+}
