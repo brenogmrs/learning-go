3ae8fcb4bfe3af6cf53d03c1a0138444c8a28c4f
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-21 16:05:41 -0300

    feat: add method to fetch ips using the net package

--- cli/app/app.go
+++ cli/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate() will return the cli ready to be used
 func Generate() *cli.App {
@@ -9,5 +15,36 @@ func Generate() *cli.App {
 	app.Name = "CLI"
 	app.Usage = "Fetch IPS and Server names"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Fetch IPs from internet addresses",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: fetchIps,
+		},
+	}
+
 	return app
+}
+
+func fetchIps(c *cli.Context) {
+
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The ips for %s are the following: \n", host)
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
