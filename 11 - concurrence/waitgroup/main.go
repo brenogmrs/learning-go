6b18d53af3b1015627efd938814b35ad3ac92808
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-22 22:27:37 -0300

    wait groupsgo run main.go !

---
+++ 11 - concurrence/waitgroup/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(2)
+
+	go func() {
+		write("Hello World!")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		write("coding in Go!")
+		waitGroup.Done()
+	}()
+
+	waitGroup.Wait()
+}
+
+func write(text string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
+}
