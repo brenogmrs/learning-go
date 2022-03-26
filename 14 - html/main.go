e44ce0a0e06a0c418a42c87884cbefb2caf05e5d
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-26 11:42:25 -0300

    loading dinamic data into html

--- 14 - html/main.go
+++ 14 - html/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,13 +9,24 @@ import (
 
 var templates *template.Template
 
+type user struct {
+	Name  string
+	Email string
+}
+
 func main() {
+
+	u := user{
+		"Jhon",
+		"jhondoe@email.com",
+	}
 
 	templates = template.Must(template.ParseGlob("*.html"))
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "index.html", nil)
+		templates.ExecuteTemplate(w, "index.html", u)
 	})
 
+	fmt.Println("Server running on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
